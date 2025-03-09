ca01c591d8fc72ed06cdaa17cf96e304b1c00954
Raul <78507802+Rahu7p@users.noreply.github.com>
2025-03-09 15:44:23 -0600

    Update main.c

--- BareMetal_C/GPIO/Src/main.c
+++ BareMetal_C/GPIO/Src/main.c
@@ -15,19 +15,19 @@ int main(void)
 {
 	/* Declarations and Initializations */
 	USER_GPIO_Init( );
-    /* Repetitive block */
-    for(;;){
-    	GPIOA->BSRR =	0x1UL <<  5U; // Set PA5 high to turn on LED
-    }
+    	/* Repetitive block */
+    	for(;;){
+    		GPIOA->BSRR =	0x1UL <<  5U; // Set PA5 high to turn on LED
+    	}
 }
 
 void USER_GPIO_Init( void ){
-  // Enable GPIOA clock
+  	// Enable GPIOA clock
 	RCC->IOPENR	  = RCC->IOPENR   |  ( 0x1UL <<  0U );
-  // Configure PA5 as output
-  GPIOA->BSRR   =	0x1UL << 21U; // Reset PA5 low to turn off LED
-  GPIOA->PUPDR  = GPIOA->PUPDR  & ~( 0x3UL << 10U ); // Clear pull-up/pull-down bits for PA5
-  GPIOA->OTYPER = GPIOA->OTYPER & ~( 0x1UL <<  5U ); // Clear output type bit for PA5
-  GPIOA->MODER  = GPIOA->MODER  & ~( 0x2UL << 10U ); // Set PA5 as output
-  GPIOA->MODER  = GPIOA->MODER  |  ( 0x1UL << 10U ); // Set PA5 as output
+  	// Configure PA5 as output
+  	GPIOA->BSRR   =	0x1UL << 21U; // Reset PA5 low to turn off LED
+  	GPIOA->PUPDR  = GPIOA->PUPDR  & ~( 0x3UL << 10U ); // Clear pull-up/pull-down bits for PA5
+  	GPIOA->OTYPER = GPIOA->OTYPER & ~( 0x1UL <<  5U ); // Clear output type bit for PA5
+  	GPIOA->MODER  = GPIOA->MODER  & ~( 0x2UL << 10U ); // Set PA5 as output
+  	GPIOA->MODER  = GPIOA->MODER  |  ( 0x1UL << 10U ); // Set PA5 as output
 }
